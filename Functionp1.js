c1253a4c37aff6b07d760849c8fbb2d14cccfd13
unknown <daveasmah@yahoo.com>
2022-05-27 16:37:17 -0400

    Correction

--- Functionp1.js
+++ Functionp1.js
@@ -4,7 +4,7 @@
 // Write a function that accepts a string as a parameter
 function findLongestWord(str) {
 
-}
+
 
 //Declare the variable for the longest word and array of words.
 let longestWord = "";
@@ -15,6 +15,7 @@ for (let i = 0; i < arraysOfWords.length ; i++) {
 	if (longestWord.length < arraysOfWords[i].length) {
 		longestWord = arraysOfWords[i];
 	} 	
+}
 }
 
 //Write and argument for the parameter
