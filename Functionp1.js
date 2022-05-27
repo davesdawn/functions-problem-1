707e5ced8ee1acb260cd8478dec31008d28e41c2
unknown <daveasmah@yahoo.com>
2022-05-26 22:49:11 -0400

    first commit

---
+++ Functionp1.js
@@ -0,0 +1,22 @@
+//Write a JavaScript function that accepts a string as a parameter and 
+//find the longest word within the string.
+
+// Write a function that accepts a string as a parameter
+function findLongestWord(str) {
+
+}
+
+//Declare the variable for the longest word and array of words.
+let longestWord = "";
+let arraysOfWords = str.split(" ");
+
+//Write a for loop with a conditional statement. 
+for (let i = 0; i < arraysOfWords.length ; i++) {
+	if (longestWord.length < arraysOfWords[i].length) {
+		longestWord = arraysOfWords[i];
+	} 	
+}
+
+//Write and argument for the parameter
+findLongestWord("This is my first JavaScript problem.")
+
